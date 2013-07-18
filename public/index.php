2b4ae78f6a3e08e7bad53320cb8a9eefbfe72013
Michael Dennis <michaeljdennis@gmail.com>
2013-07-17 18:04:07 -0700

    Add app folder, update .htaccess to redirect new gopagoda.com domains.

--- public/index.php
+++ public/index.php
@@ -4,8 +4,8 @@ require '../vendor/autoload.php';
 
 $app = new \Slim\Slim();
 
-$app->get('/', function () {
-    include('../home.php');
+$app->get('/', function() {
+    include('../app/views/home.php');
 });
 
 $app->run();
