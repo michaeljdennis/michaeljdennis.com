cd83ff14aa1a24f835ba43728f269fa9d3e30e5e
Michael Dennis <michaeljdennis@gmail.com>
2013-07-20 16:42:03 -0700

    Update jQuery to version 1.10.2

--- app/views/home.php
+++ app/views/home.php
@@ -72,7 +72,7 @@
 	
 </div>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 <script src="js/home.js"></script>
 </body>
 </html>
